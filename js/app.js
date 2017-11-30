3eeab5c64dec6428b47ad2966ce60b7c65f661cc
郭锋棉 <446227103@qq.com>
2017-12-01 02:55:28 +0800

    feat: 完成项目雏形

--- js/app.js
+++ js/app.js
@@ -1,14 +1,46 @@
 /**
  * 移动简历
  */
-
+var $window = $(window)
+var $loading = $('#loading');
+var $content = $('#content');
+var currPosition = 0;
+var prePosition = 0;
 
 var Resume = {
   init: function() {
+    var self = this;
+    scense.init();
+    me.init();
+    setTimeout(function() {
+      self.stopLoading();
+      self.bindEvent();
+    }, 4000);
     
   },
+  stopLoading() {
+    $loading.slideUp();
+  },
   bindEvent: function() {
+    // 滚动
+    $window.on('scroll', function(e) {
+      currPosition =  $window.scrollTop();
+      console.log(currPosition, prePosition);
+      // 移动距离
+      var distant	 = currPosition - prePosition;
 
+      $window.trigger('page-scroll', [currPosition, distant]);
+      prePosition = currPosition;
+    });
+
+    $content.on('click', '.js-start-resume', function() {
+      $content.css({
+        'overflow-y': 'scroll',
+        'height': 'auto'
+      });
+      $(".my-info").fadeOut(800);
+      $(".info_tip").fadeIn(500);
+    });
   }
 };
 
--- js/me.js
+++ js/me.js
@@ -1,7 +1,54 @@
+var $people;
 
 var me = {
-  move: function() {
-
+  direction: 'left',
+  init: function() {
+    $people = $('#people');
+    this.bindEvent();
+  },
+  bindEvent: function() {
+    var self = this;
+    $(window).on('page-scroll', function(e, currPosition, distant) {
+      console.log(distant);
+      if (distant > 0) {
+        console.log('right');
+        self.move('right');
+      } else {
+        console.log('left');
+        self.move('left');
+      }
+    });
+  },
+  move: function(type) {
+    if (this.isMoving) {
+      return;
+    }
+    this.direction = type;
+    var self = this;
+    var top = type === 'left' ? '-200px' : '0';
+    this.isMoving = true;
+    
+    setTimeout(function(){
+      $people.css({
+        left: '-200px',
+        top: top
+      });
+      setTimeout(function() {
+        $people.css({
+          left: '-400px',
+          top: top
+        });
+        self.stop();
+      }, 200);
+    }, 200);
+    
+  },
+  stop: function() {
+    $people.css({
+      left: 0,
+      top: this.direction === 'left' ? '-200px' : '0'
+    });
+    this.isMoving = false;
   },
   jump: function() {
 
